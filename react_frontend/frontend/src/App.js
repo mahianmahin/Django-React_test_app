85534a557877a9fa0df9d305226e32a4a2080d0f
Mahian Mahin <80479590+mahianmahin@users.noreply.github.com>
2021-10-26 19:03:06 +0600

    Update App.js

--- react_frontend/frontend/src/App.js
+++ react_frontend/frontend/src/App.js
@@ -1,46 +1,46 @@
-import { useEffect, useState } from "react";
-import Row from "./row";
+// import { useEffect, useState } from "react";
+// import Row from "./row";
 
-const App = () => {
+// const App = () => {
 
-  const url = "https://ourapi.pythonanywhere.com/api/users/"
-  // const url = "https://swapi.dev/api/people/"
+//   const url = "https://ourapi.pythonanywhere.com/api/users/"
+//   // const url = "https://swapi.dev/api/people/"
 
-  const [data, setData] = useState([]);
+//   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetchHandler();
-  }, []);
+//   useEffect(() => {
+//     fetchHandler();
+//   }, []);
   
-  const fetchHandler = () => {
-    console.log("Fetching dagta...");
-    fetch(url).then(response => {
-      return response.json();
-    }).then(result => {
-      setData(result);
-    }).catch(error => {
-      console.log(`Error: ${error}`);
-    })
-  }
+//   const fetchHandler = () => {
+//     console.log("Fetching dagta...");
+//     fetch(url).then(response => {
+//       return response.json();
+//     }).then(result => {
+//       setData(result);
+//     }).catch(error => {
+//       console.log(`Error: ${error}`);
+//     })
+//   }
 
-  console.log(data);
+//   console.log(data);
 
-  return (
-    <div>
-      <h1>Api integration</h1>
-      <button onClick={fetchHandler}>Fetch</button> <br /> <br />
-      <table border="1">
-        <th>
-          <tr>Id</tr>
-          <tr>Name</tr>
-          <tr>Email</tr>
-          <tr>City</tr>
-          <tr>Action</tr>
-        </th>
-        <Row users={data} />
-      </table>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>Api integration</h1>
+//       <button onClick={fetchHandler}>Fetch</button> <br /> <br />
+//       <table border="1">
+//         <th>
+//           <tr>Id</tr>
+//           <tr>Name</tr>
+//           <tr>Email</tr>
+//           <tr>City</tr>
+//           <tr>Action</tr>
+//         </th>
+//         <Row users={data} />
+//       </table>
+//     </div>
+//   )
+// }
 
-export default App;
+// export default App;
